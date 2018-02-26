acb49c6e85ab69eb04b533e634188bab5b3574f0
SandiaSolutions <danieleaton747@gmail.com>
2018-02-25 17:24:06 -0700

    added views

--- node-server/server.js
+++ node-server/server.js
@@ -1,29 +1,37 @@
 const express = require('express');
 let app = express();
+let hbs = require('hbs');
+
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+
+
 app.get("/",(req,res)=>{
 
-    res.send({
-        name: 'Daniel Eaton',
-        likes:[
-            'Reading',
-            'Weightlifting',
-            'Node'
-        ]
-    });
+    res.render('home.hbs', {
+        pageTitle: 'About Page Rendered',
+        currentYear: new Date().getFullYear()+1,
+        welcomeMessage: 'Welcome User'
+    })
 });
 
 
 app.get('/about',(req, res)=>{
 
-    res.send("<h1>AYYYYYYYYYYYYYYYYYYY</h1>");
+    res.render('about.hbs',{
+        pageTitle: 'About Page Rendered',
+        currentYear: new Date().getFullYear()+1
+    });
 
 });
 
 
 app.get('/bad', (req,res)=>{
    res.send({
-       errorMessage: 'Dood you fucked up doody'
+       errorMessage: 'This is representative of a bad request'
    })
 });
 
-app.listen(3000);
+app.listen(3000, ()=>{
+    console.log("Server is up on port:3000")
+});
